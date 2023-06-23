117a3cc35fd7ac5ba23e202c2d83edf6573765bf
firestige <zzt.nbl@hotmail.com>
2023-06-23 10:49:27 +0000

    调整配图和footer

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,3 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
-    injects.footer.raw('default','<span>firestige</span>')
+    injects.footer.raw('default','<div style="display: flex;justify-content: center;"><span>firestige</span></div>')
 })
